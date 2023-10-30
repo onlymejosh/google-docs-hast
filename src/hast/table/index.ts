f152f7db6ce18b465b8e26ee4b4083b57a65df5b
Josh Crowder <josh.crowder@crowdstrike.com>
2023-10-30 16:58:53 +0000

    eslint

--- src/hast/table/index.ts
+++ src/hast/table/index.ts
@@ -136,11 +136,10 @@ export const styleTableCell = (
     style.verticalAlign = contentAlignment.toLowerCase();
   }
 
+  if (columnWidth) {
+    style.width = `${columnWidth}`;
+  }
   if (Object.keys(style).length > 0) {
     el.properties.style = serializeStyle(style);
-  }
-
-  if (columnWidth) {
-    style.width = `${columnWidth}%`;
   }
 };
